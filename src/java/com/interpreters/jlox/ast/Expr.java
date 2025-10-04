1196853b2c8ab74fee96e66e8a9a5be827d61317
gvsalinas <gvsalinas3@gmail.com>
2025-10-04 00:04:35 -0500

    "AstPrinter using Visitor design"

--- src/java/com/interpreters/jlox/ast/Expr.java
+++ src/java/com/interpreters/jlox/ast/Expr.java
@@ -1,42 +1,76 @@
 package com.interpreters.jlox.ast;
 
 public abstract class Expr {
+
+    public interface Visitor<R> {
+        R visitBinaryExpr(Binary expr);
+        R visitGroupingExpr(Grouping expr);
+        R visitLiteralExpr(Literal expr);
+        R visitUnaryExpr(Unary expr);
+    }
+
+    public abstract <R> R accept(Visitor<R> visitor);
+
     public static class Binary extends Expr {
 
-        private final Expr left;
-        private final Token operator;
-        private final Expr right;
+        public final Expr left;
+        public final Token operator;
+        public final Expr right;
 
         public Binary(Expr left, Token operator, Expr right) {
             this.left = left;
             this.operator = operator;
             this.right = right;
         }
+
+        @Override
+        public <R> R accept(Visitor<R> visitor) {
+            return visitor.visitBinaryExpr(this);
+        }
     }
+
     public static class Grouping extends Expr {
 
-        private final Expr expression;
+        public final Expr expression;
 
         public Grouping(Expr expression) {
             this.expression = expression;
         }
+
+        @Override
+        public <R> R accept(Visitor<R> visitor) {
+            return visitor.visitGroupingExpr(this);
+        }
     }
+
     public static class Literal extends Expr {
 
-        private final Object value;
+        public final Object value;
 
         public Literal(Object value) {
             this.value = value;
         }
+
+        @Override
+        public <R> R accept(Visitor<R> visitor) {
+            return visitor.visitLiteralExpr(this);
+        }
     }
+
     public static class Unary extends Expr {
 
-        private final Token operator;
-        private final Expr right;
+        public final Token operator;
+        public final Expr right;
 
         public Unary(Token operator, Expr right) {
             this.operator = operator;
             this.right = right;
         }
+
+        @Override
+        public <R> R accept(Visitor<R> visitor) {
+            return visitor.visitUnaryExpr(this);
+        }
     }
+
 }
--- src/java/com/interpreters/jlox/ast/GenerateAST.java
+++ src/java/com/interpreters/jlox/ast/GenerateAST.java
@@ -31,6 +31,13 @@ public class GenerateAST {
             writer.println("import java.util.List;");
             writer.println();
             writer.println("public abstract class " + baseName + " {");
+
+            // The base accept() method.
+            defineVisitor(writer, baseName, types);
+            writer.println();
+            writer.println("    public abstract <R> R accept(Visitor<R> visitor);");
+            writer.println();
+
             for (String type : types) {
                 String className = type.split(":")[0].trim();
                 String fields = type.split(":")[1].trim();
@@ -47,19 +54,41 @@ public class GenerateAST {
         String[] fields = fieldList.split(", ");
         writer.println();
         for (String field : fields) {
-            writer.println("        private final " + field + ";");
+            writer.println("        public final " + field + ";");
         }
 
         // Constructor.
         writer.println();
         writer.println("        public " + className + "(" + fieldList + ") {");
+
         // Store parameters in fields.
         for (String field : fields) {
             String name = field.split(" ")[1];
             writer.println("            this." + name + " = " + name + ";");
         }
-
         writer.println("        }");
+
+        // Visitor pattern.
+        writer.println();
+        writer.println("        @Override");
+        writer.println("        public <R> R accept(Visitor<R> visitor) {");
+        writer.println("            return visitor.visit" + className + baseName + "(this);");
+        writer.println("        }");
+
+        writer.println("    }");
+        writer.println();
+    }
+
+    private static void defineVisitor(PrintWriter writer, String baseName, List<String> types) {
+        writer.println();
+        writer.println("    public interface Visitor<R> {");
+
+        for (String type : types) {
+            String typeName = type.split(":")[0].trim();
+            writer.println("        R visit" + typeName + baseName + "(" +
+                    typeName + " " + baseName.toLowerCase() + ");");
+        }
+
         writer.println("    }");
     }
 }
